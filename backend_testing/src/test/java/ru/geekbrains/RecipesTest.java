3059a6c4f2cc2dc2f190a47d577d646c36e17f0b
Aleksandr Melekhin <85239026+Melekhin-ao@users.noreply.github.com>
2022-05-26 15:59:32 +0300

    Add files via upload

--- backend_testing/src/test/java/ru/geekbrains/RecipesTest.java
+++ backend_testing/src/test/java/ru/geekbrains/RecipesTest.java
@@ -1,33 +1,15 @@
 package ru.geekbrains;
 
-import io.restassured.RestAssured;
-import net.javacrumbs.jsonunit.JsonAssert;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-
 import static io.restassured.RestAssured.given;
-import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
 import static org.hamcrest.Matchers.*;
 
-public class RecipesTest {
-    private static final String API_KEY = "a6db1d1b10de42a29ddd8513df5fd950";
-
-    @BeforeAll
-    static void beforeAll() {
-        RestAssured.baseURI = "https://api.spoonacular.com/recipes/";
-    }
+public class RecipesTest extends SpoonaccularTest {
 
     @Test
-    void testRecipeInfoSearch() throws IOException {
+    void testRecipeInfoSearch() throws Exception {
         String actually = given()
-                .log()
-                .all()
-                .param("apiKey", API_KEY)
                 .pathParam("id", 716429)
                 .expect()
                 .body("vegetarian", is(false))
@@ -40,71 +22,47 @@ public class RecipesTest {
                 .body("sustainable", is(false))
                 .body("lowFodmap", is(false))
                 .when()
-                .get("{id}/information")
+                .get("recipes/{id}/information")
                 .body()
                 .prettyPrint();
 
-        String expected = getResourceAsString("RecipeInfoExpected.json");
-
-        JsonAssert.assertJsonEquals(
-                expected,
-                actually,
-                JsonAssert.when(IGNORING_ARRAY_ORDER)
-        );
+        String expected = getResource("RecipeInfoExpected.json");
+        assertJson(expected, actually);
     }
 
     @Test
-    void testSimilarRecipesSearch() throws IOException {
+    void testSimilarRecipesSearch() throws Exception {
 
         String actually = given()
-                .log()
-                .all()
-                .param("apiKey", API_KEY)
                 .param("number", 1)
                 .pathParam("id", 715538)
                 .expect()
                 .when()
-                .get("{id}/similar")
+                .get("recipes/{id}/similar")
                 .prettyPrint();
 
-        String expected = getResourceAsString("SimilarRecipesExpected.json");
-
-        JsonAssert.assertJsonEquals(
-                expected,
-                actually,
-                JsonAssert.when(IGNORING_ARRAY_ORDER)
-        );
+        String expected = getResource("SimilarRecipesExpected.json");
+        assertJson(expected, actually);
     }
 
     @Test
-    void testPriceBreakdown() throws IOException {
+    void testPriceBreakdown() throws Exception {
 
         String actually = given()
-                .log()
-                .all()
-                .param("apiKey", API_KEY)
                 .pathParam("id", 1003464)
                 .expect()
                 .when()
-                .get("{id}/priceBreakdownWidget.json")
+                .get("recipes/{id}/priceBreakdownWidget.json")
                 .prettyPrint();
 
-        String expected = getResourceAsString("PriceBreakdownExpected.json");
-
-        JsonAssert.assertJsonEquals(
-                expected,
-                actually,
-                JsonAssert.when(IGNORING_ARRAY_ORDER)
-        );
+        String expected = getResource("PriceBreakdownExpected.json");
+        assertJson(expected, actually);
     }
 
     @Test
-    void testNutritionByIdSearch() throws IOException {
+    void testNutritionByIdSearch() throws Exception {
 
         String actually = given()
-                .log()
-                .all()
-                .param("apiKey", API_KEY)
                 .pathParam("id", 1003464)
                 .expect()
                 .body("calories", is("316k"))
@@ -112,38 +70,23 @@ public class RecipesTest {
                 .body("fat", is("12g"))
                 .body("protein", is("3g"))
                 .when()
-                .get("{id}/nutritionWidget.json")
+                .get("recipes/{id}/nutritionWidget.json")
                 .prettyPrint();
 
-        String expected = getResourceAsString("NutritionByIdExpected.json");
-
-        JsonAssert.assertJsonEquals(
-                expected,
-                actually,
-                JsonAssert.when(IGNORING_ARRAY_ORDER)
-        );
+        String expected = getResource("NutritionByIdExpected.json");
+        assertJson(expected, actually);
     }
 
-    @Test
-    void testSummarizeRecipeSearch() throws IOException {
-
-        given()
-                .log()
-                .all()
-                .param("apiKey", API_KEY)
-                .pathParam("id", 4632)
-                .expect()
-                .body("id", is(4632))
-                .body("title", is("Soy-and-Ginger-Glazed Salmon with Udon Noodles"))
-                .when()
-                .get("{id}/summary")
-                .prettyPrint();
+        @Test
+        void testSummarizeRecipeSearch () throws Exception {
+            given()
+                    .pathParam("id", 4632)
+                    .expect()
+                    .body("id", is(4632))
+                    .body("title", is("Soy-and-Ginger-Glazed Salmon with Udon Noodles"))
+                    .when()
+                    .get("recipes/{id}/summary")
+                    .prettyPrint();
+        }
     }
 
-    public String getResourceAsString(String resource) throws IOException {
-        InputStream stream = getClass().getResourceAsStream(resource);
-        assert stream != null;
-        byte[] bytes = stream.readAllBytes();
-        return new String(bytes, StandardCharsets.UTF_8);
-    }
-}
---
+++ backend_testing/src/test/java/ru/geekbrains/SpoonaccularTest.java
@@ -0,0 +1,29 @@
+package ru.geekbrains;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import org.junit.jupiter.api.BeforeAll;
+
+public class SpoonaccularTest extends AbstractTest {
+
+    private static final String API_KEY = "a6db1d1b10de42a29ddd8513df5fd950";
+
+    @BeforeAll
+    static void beforeAll() {
+
+        RestAssured.baseURI = "https://api.spoonacular.com/";
+
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .addQueryParam("apiKey", API_KEY)
+                .log(LogDetail.ALL)
+                .build();
+
+        RestAssured.responseSpecification = new ResponseSpecBuilder()
+                .log(LogDetail.BODY)
+                .build();
+    }
+
+
+}
