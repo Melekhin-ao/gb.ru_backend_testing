09a2d74facea843a9753e547e5aeb996cfe7983f
Aleksandr Melekhin <85239026+Melekhin-ao@users.noreply.github.com>
2022-06-08 20:13:31 +0300

    Add files via upload

---
+++ minimarket/src/main/java/ru/geekbrains/minimarket/SerializeJson.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.minimarket;
+
+import com.google.gson.Gson;
+
+public class SerializeJson {
+    private static final Gson gson = new Gson();
+    public static <T> String getJson(T object) {
+        return gson.toJson(object);
+    }
+
+    public static <T> T getObject(String json, Class<T> clazz) {
+        return gson.fromJson(json, clazz);
+    }
+}
